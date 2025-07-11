9d939fa9ec4292ec9c5319122d04ad869644058c
unknown <mayurikhose09@gmail.com>
2025-07-11 17:54:09 +0530

    added files

--- client/src/store/slices/authSlice.js
+++ client/src/store/slices/authSlice.js
@@ -17,7 +17,7 @@ import {
 import { auth, cleanupRecaptcha } from "../../config/firebase"
 import axios from "axios"
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api"
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 // Create axios instance
 const api = axios.create({
